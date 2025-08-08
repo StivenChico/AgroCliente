2a9970c887a4c3843113573fe2e0ed6a07f604e7
Moises Solis <moisessolis.gow@hotmail.com>
2025-08-08 18:47:32 -0500

    uno

---
+++ AgroCliente/src/main/java/com/mycompany/agrocliente/AgroCliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agrocliente;
+
+/**
+ *
+ * @author moises-solis
+ */
+public class AgroCliente {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
