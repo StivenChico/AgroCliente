b94b2a76ffc90fa01342cb63c3fcdbe66b8afb0d
estudiante <estudiante@EA12-11682003.itsa.edu.local>
2025-08-08 19:16:58 -0500

    FIX

---
+++ src/main/java/com/mycompany/AgroCliente/Clientes.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.AgroCliente;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Clientes {
+    private String Nombre;
+    private String Apellido;
+    private String Documento;
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Apellido
+     */
+    public String getApellido() {
+        return Apellido;
+    }
+
+    /**
+     * @param Apellido the Apellido to set
+     */
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    /**
+     * @return the Documento
+     */
+    public String getDocumento() {
+        return Documento;
+    }
+
+    /**
+     * @param Documento the Documento to set
+     */
+    public void setDocumento(String Documento) {
+        this.Documento = Documento;
+    }
+    
+}
